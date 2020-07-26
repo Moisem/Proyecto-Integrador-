1806d18a63061b1db3ad1fb4730af2ec4974110a
JoelGutierrezN <al221910321@gmail.com>
2020-07-26 15:32:19 -0500

    correcciones minimas

--- Direccion.php
+++ Direccion.php
@@ -1,3 +1,4 @@
+<?php include_once "includes/templates/header.php";?>
 <form action="" method="post">
 <fieldset class="direccion">
         <legend>Direcciones de Envio</legend>
@@ -5,55 +6,13 @@
             <label for="estado">Estado</label>
             <select id="estado">
                 <option value="" selected disabled>-Seleccionar-</option>
-                <?php
-                    try{
-                        require_once("includes/funciones/BD_conexion.php");
-                        $sql = "SELECT * FROM estado";
-                        $resultado = $conn->query($sql);
-                    }catch (\Exception $e){
-                        echo $e->getMessage();
-                    }
-                ?>
-                
-                <?php
-                    while($estados = $resultado->fetch_assoc()){?>
-                        <option value="" name="estado">
-                            <?php
-                                echo $estados['nombre'];
-                            }?>
-                        </option>
+                <option value="" name="estado"></option>
             </select>
             
             <label for="municipio">Municipio</label>
             <select name="municipio" id="municipio">
             <option value="" selected disabled>-Seleccionar-</option>
-            <?php
-                    try{
-                        require_once("includes/funciones/BD_conexion.php");
-                        $sql = "SELECT * FROM municipios";
-                        $resultado = $conn->query($sql);
-                    }catch (\Exception $e){
-                        echo $e->getMessage();
-                    }
-                ?>
-                
-                <?php
-                while($muni = $resultado->fetch_assoc()){
-                    if($estado['id']=$muni['estado_id'])
-                        {
-                        while($mun = $resultado->fetch_assoc()){?>
-                            <option value="">
-                        <?php
-                                echo $mun['nombre'];
-                            }
-                        }else{
-                            echo "<h2>ERROR<h2>";
-                        }
-                    }?>
-                            </option>
-                <?php
-                    $conn->close();
-                ?>
+            <option value=""></option>
             </select>
             <label for="calle">Calle</label>
             <input type="text" id="calle" placeholder="Ingrese su calle">
@@ -71,3 +30,5 @@
             <input type="submit"  value="Agregar Direccion de Envio" class="boton boton-azul-cielo">
         </div>
 </form>
+</body>
+</html>
